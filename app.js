cb81deb43ddb20ec638a636338e740ab382f6c51
farhat sattar <farhatsattar8675@gmail.com>
2024-06-04 15:59:13 +0500

    calculator -proj

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-"use strict";
-console.log("step00_helloworld");
+import inquirer from "inquirer";
+import { subtraction, sum, division, multiplication } from "./operations.js";
+const questions = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter your first number",
+    },
+    { type: "number",
+        name: "num2",
+        message: "Enter your second number",
+    },
+    { type: "list",
+        name: "operator",
+        message: "select your operator",
+        choices: ["-", "+", "/", "*"],
+    }
+]);
+console.log("questions:", questions);
+if (questions.operator == "-") {
+    console.log("this is subtraction");
+    subtraction(questions.num1, questions.num2);
+}
+else if (questions.operator == "+") {
+    console.log("this is sum");
+    sum(questions.num1, questions.num2);
+}
+else if (questions.operator == "/") {
+    console.log("this is division");
+    division(questions.num1, questions.num2);
+}
+else if (questions.operator == "*") {
+    console.log("this is multiplication");
+    multiplication(questions.num1, questions.num2);
+}
+else {
+    console.log("invalid opertaion");
+}
---
+++ operations.js
@@ -0,0 +1,17 @@
+function subtraction(num1, num2) {
+    const result = num1 - num2;
+    console.log(`subtraction of ${num1} and ${num2} = ${result}`);
+}
+function sum(num1, num2) {
+    const result = num1 + num2;
+    console.log(`sum of ${num1} and ${num2} = ${result}`);
+}
+function division(num1, num2) {
+    const result = num1 / num2;
+    console.log(`division of ${num1} and ${num2} = ${result}`);
+}
+function multiplication(num1, num2) {
+    const result = num1 * num2;
+    console.log(`multiplication of ${num1} and ${num2} = ${result}`);
+}
+export { subtraction, sum, division, multiplication };
